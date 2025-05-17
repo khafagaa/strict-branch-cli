de3f326c63ee0d800b7f19504a6cd05af0c4424b
khafagaa <ihabkhafagaaa@gmail.com>
2025-05-17 13:57:20 +0300

    publish stable version

--- bin/index.js
+++ bin/index.js
@@ -3,6 +3,3 @@ import("../dist/cli.js").catch(err => {
   console.error("Failed to load CLI:", err);
   process.exit(1);
 });
-
-//npx javascript-obfuscator src --output dist --config obfuscator-config.json
-//npm pack
